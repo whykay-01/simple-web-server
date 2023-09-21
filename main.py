1e58c0b475db4b8d1744971692191b5168056327
Yan <100032599+whykay-01@users.noreply.github.com>
2023-09-21 13:22:32 +0800

    improved robustness of the server

--- main.py
+++ main.py
@@ -14,8 +14,21 @@ while True:
     print('----- Connection established -----')
     try:
         message = connectionSocket.recv(1024)
-        filename = message.split()[1]
         
+        if not message:
+            continue  # Ignore empty requests
+        
+        # Split the message into lines and extract the request line
+        request_lines = message.split(b'\r\n')
+        request_line = request_lines[0].decode('utf-8')
+        
+        # Check if the request is valid
+        if not request_line.startswith('GET'):
+            continue  # Ignore non-GET requests
+        
+        # Extract the requested filename
+        filename = request_line.split()[1]
+        print('----- Requested filename: ' + filename[1:] + ' -----')
         f = open(filename[1:])
         outputdata = f.read()
 
@@ -36,6 +49,11 @@ while True:
         
         # Close client socket
         connectionSocket.close()
+    
+    except:
+        print('----- Connection closed due to unexpected error! -----')
+        connectionSocket.close()
+        break
 
 serverSocket.close()
 sys.exit()  # Terminate the program after sending the corresponding data
